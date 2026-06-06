853c6b1c94d5ded31915db93918cada4131633c2
Ingkar Adilbek <190526775+daaingkaryaad@users.noreply.github.com>
2026-06-06 19:35:01 +0500

    feat: add home endpoint

---
+++ github-analytics-backend/src/main/java/com/githubanalytics/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.githubanalytics.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "GitHub Analytics API is running";
+    }
+}
